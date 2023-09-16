5164c76aa7715219b27a9694e0f30b110de961ee
GeralBot <133059729+GeralBot@users.noreply.github.com>
2023-09-15 19:34:10 -0500

    a

--- plugins/s-amor.js
+++ plugins/s-amor.js
@@ -2,8 +2,8 @@ import { sticker } from '../lib/sticker.js'
 let handler = async(m, { conn }) => {
 if (!db.data.chats[m.chat].stickers && m.isGroup) throw 0
 
-let nombre = '🐈 𝐌𝐚𝐭𝐞𝐨 𝐁𝐨𝐭'
-let nombre2 = '𝐌𝐚𝐭𝐞𝐨 𝐁𝐨𝐭' 
+let nombre = '🐈 𝑮𝒊𝒂𝒏𝒊𝒕𝒂'
+let nombre2 = '𝑮𝒊𝒂𝒏𝒊𝒕𝒂' 
  
 const s = [
 'https://media1.giphy.com/media/2dQ3FMaMFccpi/giphy.gif?cid=ecf05e476azkdvh2cu7b567gbpgyc6q7qd38pklqp12npygv&rid=giphy.gif&ct=g',
